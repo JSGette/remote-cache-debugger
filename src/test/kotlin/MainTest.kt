8c8d1555e75b8a71fbfd271740bea5497af4e35b
Joseph Gette <jgettepost@gmail.com>
2023-04-01 22:36:08 +0200

    Initial structure for unit tests

---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test
+
+class MainTest {
+
+    @Test
+    fun `Calculated SHA-256 hash is correct`() {
+
+    }
+
+    @Test
+    fun `calculateDiff returns matched keys with different values`() {
+
+    }
+
+    @Test
+    fun `calculateDiff returns orphaned keys present in aMap but not in bMap`() {
+
+    }
+
+    @Test
+    fun `calculateDiff returns orphaned keys present in bMap but not in aMap`() {
+
+    }
+
+    @Test
+    fun `mergeSpawnExecs correctly merges 2 lists of SpawnExecs`() {
+        //produce output binary log, parse it and check the correctness
+    }
+}
