01f8084cc4bbe2da38826a700506f0fb0bbed998
Joseph Gette <jgettepost@gmail.com>
2023-04-01 16:25:11 +0200

    Use output.proto to produce MergedSpawnExec

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import com.gette.debugger.Protos
+import com.gette.debugger.Protos.ExecutionEnvironmentVariables
+import com.gette.debugger.Protos.ExecutionInputs
 import com.google.devtools.build.lib.exec.Protos.SpawnExec
 import java.io.File
 import java.io.InputStream
@@ -10,57 +13,56 @@ fun main(args: Array<String>) {
 }
 
 fun mergeSpawnExecs(pathA: String, pathB: String) {
-    val mergedSpawnExecs: HashMap<String, MergedSpawnExec> = HashMap()
+    val aExecCounter: Int
+    var bExecCounter: Int = 0
+    var cacheHits: Int = 0
+    val aSpawnExecs: HashMap<String, SpawnExec> = HashMap()
     var ins = File(pathA).inputStream()
-
     while (ins.available() > 0) {
         val spawnExec = getNextSpawnExec(ins)
-        val mergedSpawnExec: MergedSpawnExec =
-            MergedSpawnExec(
-                spawnExec.second.listedOutputsList,
-                spawnExec.second.environmentVariablesList.associate { it.name to it.value },
-                spawnExec.second.inputsList.associate { it.path to it.digest }
-            )
-        mergedSpawnExecs[spawnExec.first] = mergedSpawnExec
+        aSpawnExecs[spawnExec.first] = spawnExec.second
     }
+    aExecCounter = aSpawnExecs.size
 
     ins = File(pathB).inputStream()
     while (ins.available() > 0) {
         val spawnExec = getNextSpawnExec(ins)
-        if (mergedSpawnExecs.contains(spawnExec.first)) {
-            mergedSpawnExecs[spawnExec.first]!!.presentInBothExecs = true
-            mergedSpawnExecs[spawnExec.first]!!.bEnvVars =
+        bExecCounter++
+        if (!spawnExec.second.remoteCacheHit) {
+            val aEnvVars: Map<String, String> =
+                aSpawnExecs[spawnExec.first]!!.environmentVariablesList.associate { it.name to it.value }
+            val bEnvVars: Map<String, String> =
                 spawnExec.second.environmentVariablesList.associate { it.name to it.value }
-            mergedSpawnExecs[spawnExec.first]!!.bInputs = spawnExec.second.inputsList.associate { it.path to it.digest }
+            val mergedEnvVars =
+                calculateDiff(aEnvVars, bEnvVars).map {
+                    ExecutionEnvironmentVariables.newBuilder().setName(it.key).setAValue(it.value.first)
+                        .setBValue(it.value.second).build()
+                }
+            val aInputs = aSpawnExecs[spawnExec.first]!!.inputsList.associate { it.path to it.digest }
+            val bInputs = spawnExec.second.inputsList.associate { it.path to it.digest }
+            val mergedInputs = calculateDiff(aInputs, bInputs).map {
+                ExecutionInputs.newBuilder().setPath(it.key).setAHash(it.value.first.hash)
+                    .setBHash(it.value.second.hash).build()
+            }
+            var mergedSpawnExec =
+                Protos.MergedSpawnExec.newBuilder().setExecutionHash(spawnExec.first)
+                    .addAllListedOutputs(spawnExec.second.listedOutputsList)
+                    .addAllEnvVars(mergedEnvVars.toMutableList())
+                    .addAllInputs(mergedInputs.toMutableList())
+            println(mergedSpawnExec.toString())
+
         } else {
-            mergedSpawnExecs[spawnExec.first] = MergedSpawnExec(
-                spawnExec.second.listedOutputsList,
-                HashMap(),
-                HashMap(),
-                spawnExec.second.environmentVariablesList.associate { it.name to it.value },
-                spawnExec.second.inputsList.associate { it.path to it.digest },
-                false
-            )
+            cacheHits++
         }
     }
-    File("D:/output.txt").printWriter().use { out ->
-        mergedSpawnExecs.forEach {
-
-            out.println("=============================")
-            out.println("Listed Outputs {")
-            it.value.listedOutputs.forEach { listedOutput -> out.println("  $listedOutput") }
-            out.println("}")
-            it.value.printEnvVarsDiff(out)
-            it.value.printInputsDiff(out)
-
-            /* println("=============================")
-             println("Listed Outputs {")
-             it.value.listedOutputs.forEach { listedOutput -> println("  ${listedOutput}") }
-             println("}")
-             it.value.printEnvVarsDiff()
-             it.value.printInputsDiff()*/
-        }
+    if (aExecCounter != bExecCounter) {
+        throw UnsupportedOperationException("Number of executions isn't the same. Unsupported analysis!")
     }
+    println("====================REPORT====================")
+    println("Spawned Executions: ${aExecCounter}")
+    println("Cache Hits: ${cacheHits}")
+    println("Cache Hit Rate: ${"%.2f".format(cacheHits.toFloat() / aExecCounter.toFloat() * 100)}%")
+    println("==============================================")
 }
 
 fun getNextSpawnExec(ins: InputStream): Pair<String, SpawnExec> {
@@ -72,4 +74,11 @@ fun getNextSpawnExec(ins: InputStream): Pair<String, SpawnExec> {
 fun calculateExecHash(input: String): String {
     return sha256.digest(input.toByteArray())
         .fold("") { str, it -> str + "%02x".format(it) }
+}
+
+fun <T> calculateDiff(aMap: Map<String, T>, bMap: Map<String, T>): Map<String, Pair<T, T>> {
+    return aMap.filterKeys { bMap.containsKey(it) }.filter { (key, value) ->
+        aMap[key] != bMap[key]
+    }.map { Triple(it.key, aMap[it.key], bMap[it.key]) }.associate { it.first to Pair(it.second!!, it.third!!) }
+        .filter { it.value.toList().isNotEmpty() }
 }
