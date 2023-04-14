83fa7a9d0059db11c3df86232237c9bbcf9c73b3
Joseph Gette <jgettepost@gmail.com>
2023-04-14 15:38:32 +0200

    Add unit test for calculateDiff

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -7,16 +7,23 @@ class MainTest {
     fun `Calculated SHA-256 hash is correct`() {
         val testString = "testStringToCalculateSHA256Hash"
         val expectedHash = "9794754e527299b8d3a53bdc825f63821da5841a6f7adfdac16570435231879e"
-        val calculatedHash = calculateExecHash(testString)
-        assertEquals(expectedHash, calculatedHash)
+        val actualHash = calculateExecHash(testString)
+        assertEquals(expectedHash, actualHash)
     }
 
     @Test
     fun `calculateDiff returns matched keys with different values`() {
-
+        val sameValues = hashMapOf("b" to "valB", "c" to "valC")
+        val aMap = hashMapOf("a" to "valA")
+        aMap.putAll(sameValues)
+        val bMap = hashMapOf("a" to "valA2")
+        bMap.putAll(sameValues)
+        val actualDiffMap = calculateDiff(aMap, bMap)
+        val expectedDiffMap = hashMapOf("a" to Pair("valA", "valA2"))
+        assertEquals(expectedDiffMap, actualDiffMap)
     }
 
-    @Test
+/*    @Test
     fun `calculateDiff returns orphaned keys present in aMap but not in bMap`() {
 
     }
@@ -29,5 +36,5 @@ class MainTest {
     @Test
     fun `mergeSpawnExecs correctly merges 2 lists of SpawnExecs`() {
         //produce output binary log, parse it and check the correctness
-    }
+    }*/
 }
